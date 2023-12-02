ec31053a4ade4c03234228be7bf674d6e2ce70b4
noormanyulizarp <noormanyulizarp@gmail.com>
2023-12-02 18:12:23 +0000

    Initial commit

---
+++ src/Cloud.tsx
@@ -0,0 +1,13 @@
+// src/Cloud.tsx
+import React from 'react';
+
+const Cloud: React.FC = () => {
+  return (
+    <div>
+      <h2>Cloud Component ☁️</h2>
+      <p>This is the Cloud component content. You can add specific features related to cloud services here.</p>
+    </div>
+  );
+};
+
+export default Cloud;
---
+++ src/Downloader.tsx
@@ -0,0 +1,13 @@
+// src/Downloader.tsx
+import React from 'react';
+
+const Downloader: React.FC = () => {
+  return (
+    <div>
+      <h2>Downloader Component 📥</h2>
+      <p>This is the Downloader component content. You can add specific features related to downloading here.</p>
+    </div>
+  );
+};
+
+export default Downloader;
