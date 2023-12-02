63d025eac44e6780d3b1931f1f8aeb9c7e7cfc00
noormanyulizarp <noormanyulizarp@gmail.com>
2023-12-02 18:22:01 +0000

    Refactor App.tsx with new component Homepage.tsx
    Create new component for Homepage.tsx

---
+++ src/Homepage.tsx
@@ -0,0 +1,46 @@
+// src/Homepage.tsx
+import React, { useState } from 'react';
+import Downloader from './Downloader';
+import Cloud from './Cloud';
+
+const Homepage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<'downloader' | 'cloud'>('downloader');
+
+  const handleTabChange = (tab: 'downloader' | 'cloud') => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <main className="container mt-4">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === 'downloader' ? 'active' : ''}`}
+            onClick={() => handleTabChange('downloader')}
+          >
+            Downloader
+          </button>
+        </li>
+        <li className="nav-item">
+          <button
+            className={`nav-link ${activeTab === 'cloud' ? 'active' : ''}`}
+            onClick={() => handleTabChange('cloud')}
+          >
+            Cloud
+          </button>
+        </li>
+      </ul>
+
+      <div className="tab-content mt-3">
+        <div className={`tab-pane fade ${activeTab === 'downloader' ? 'show active' : ''}`}>
+          <Downloader />
+        </div>
+        <div className={`tab-pane fade ${activeTab === 'cloud' ? 'show active' : ''}`}>
+          <Cloud />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Homepage;
